909a1133e5ad0b5b55e625250574da9a4c6a28bb
Chandanku123 <chandanpandeyprog@gmail.com>
2022-08-25 23:48:59 +0530

    Sort array

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿public class SortingArray
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = { 5,2,8,7,1};
+
+        for (int i = 0; i < arr.Length; i++) {   
+            for (int j = i+1; j < arr.Length; j++) {   
+               if(arr[i] > arr[j]) {  
+                  int temp = arr[i];  
+                   arr[i] = arr[j];  
+                   arr[j] = temp;  
+               }   
+            }   
+        }  
+       for (int i = 0; i < arr.Length; i++) {   
+            Console.Write(arr[i] + " ");  
+        }  
+       
+    }
+}
